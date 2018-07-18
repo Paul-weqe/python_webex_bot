95f12ec19b8fe410f6d413db89e3b202fba3aa80
Paul-weqe <paul1tw1@gmail.com>
2018-07-18 23:05:46 +0300

    Starting the Spark class

---
+++ spark.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+class Spark:
+    
+    def __init__(self, access_token):
+        requests.packages.urllib3.disable_warnings()
+        # url and the parameters
+        self.url = "https://api.ciscospark.com/v1"
+        self.access_token = "Bearer " + access_token
+        return None
+    
+    # send message to a specific party
+    def sendMessage(self, room_id, message):
+        api_call = "/messages"
+        url = self.url + api_call
+        headers = {
+            "Content-Type": "application/json",
+            "authorization": self.access_token
+        }
+        json = message
+        response = requests.post(url, json={"roomId":room_id, "text":message}, headers=headers)
+        print(response.text)
+        return None
+    
+    
+    
+    
---
+++ tests.py
@@ -0,0 +1,7 @@
+from spark import Spark
+
+sp = Spark("your_acces_token")
+
+# sending message test
+# sp.sendMessage("roomId", "Awesome")
+
