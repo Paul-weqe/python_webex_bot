cc64a7f8b9eef30c0ba123ba7bf04df11ea7338c
Paul-weqe <paul1tw1@gmail.com>
2018-08-13 16:38:04 +0300

    polo

--- test.py
+++ test.py
@@ -43,13 +43,6 @@ def respond_to_hi():
 	room_id = myBot.message_data["roomId"]
 	myBot.sendMessage(room_id, "hi there too :)")
 
-@myBot.onHears("Sarri")
-def respond_to_sarri():
-	print("Sarriball")
-	room_id = myBot.message_data["roomId"]
-	myBot.send_attachment(room_id, "https://cdn.images.express.co.uk/img/dynamic/67/590x/Chelsea-team-news-1002186.jpg?r=1534028634745")
-	myBot.sendMessage(room_id, "There you go")
-
 @myBot.onHears("Cool", mention=True)
 def respond_to_cool_mention():
 	room_id = myBot.message_data["roomId"]
