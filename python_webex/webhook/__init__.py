d12b98f850f27839ea739c6d91e158297387e22b
Paul-weqe <paul1tw1@gmail.com>
2020-05-11 11:17:30 +0300

    Fix readme problem on run.py script

--- python_webex/webhook/__init__.py
+++ python_webex/webhook/__init__.py
@@ -8,7 +8,6 @@ bot = None
 def index():
 
     json_data = request.get_json()
-    
     message_id = json_data[ "data" ][ "id" ]
     message_info = bot.get_message_details( message_id=message_id ).json()
 
