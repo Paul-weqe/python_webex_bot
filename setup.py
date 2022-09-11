2cc2567f4120549fca5d09020c03e4a32a58140c
weqe <paul1tw1@gmail.com>
2022-09-11 05:58:52 +0300

    Upgrade to latest version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python_webex_bot",
-    version="0.90",
+    version="0.900",
 
     scripts=["python_webex_bot"],
     author="Paul-weqe",
