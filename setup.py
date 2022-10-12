84c942844411c2255249397583878775cb823da1
weqe <paul1tw1@gmail.com>
2022-10-12 22:43:11 +0300

    Upgrade library version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python_webex_bot",
-    version="0.900",
+    version="0.901",
 
     scripts=["python_webex_bot"],
     author="Paul-weqe",
